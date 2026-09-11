c7e16c769ca7fce732bf8105ab8564f871a677b3
DEX-7 <manishkafle03@gmail.com>
2026-09-11 22:00:52 +0545

    Add PasteFactory for database seeding and testing

---
+++ database/factories/PasteFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Paste;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Paste>
+ */
+class PasteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
